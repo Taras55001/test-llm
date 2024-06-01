e8545fd0a6093a7087543850ea2e2e8fc3ff59e8
taras55001 <taras5500@gmail.com>
2024-06-01 10:02:22 +0200

    second

--- test.py
+++ test.py
@@ -1,74 +1,42 @@
-import pyaudio
-import wave
+# from transformers import AutoTokenizer, pipeline
+# import torch
+from huggingface_hub import login
+HF_TOKEN = "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"
+login(token=HF_TOKEN, add_to_git_credential=True)
+# model = "google/gemma-2b"
+# pipeline = pipeline(
+#     "text-generation",
+#     model=model,
+#     model_kwargs={
+#         "torch_dtype": torch.float16,
+#         "quantization_config": {"load_in_4bit": True}
+#     }, token=HF_TOKEN
+# )
+#
+# messages = [
+#     {"role": "user", "content": "Tell me about ChatGPT"},
+# ]
+# prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+# outputs = pipeline(
+#     prompt,
+#     max_new_tokens=256,
+#     do_sample=True,
+#     temperature=0.7,
+#     top_k=50,
+#     top_p=0.95
+# )
+# print(outputs[0]["generated_text"][len(prompt):])
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+model = AutoModelForCausalLM.from_pretrained(
+    "google/gemma-2b",
+    torch_dtype=torch.bfloat16
+)
 
-def list_microphones():
-    p = pyaudio.PyAudio()
-    info = p.get_host_api_info_by_index(0)
-    num_devices = info.get('deviceCount')
+input_text = "Write me a poem about CNC Machinist."
+input_ids = tokenizer(input_text, return_tensors="pt")
 
-    microphones = []
-
-    for i in range(num_devices):
-        device_info = p.get_device_info_by_host_api_device_index(0, i)
-        if device_info.get('maxInputChannels') > 0:
-            microphones.append((i, device_info.get('name')))
-
-    p.terminate()
-
-    return microphones
-
-
-def record_audio(device_index, duration=5, filename="output.wav"):
-    p = pyaudio.PyAudio()
-
-    # Налаштування параметрів запису
-    format = pyaudio.paInt16
-    channels = 1
-    rate = 44100
-    frames_per_buffer = 1024
-
-    stream = p.open(format=format,
-                    channels=channels,
-                    rate=rate,
-                    input=True,
-                    input_device_index=device_index,
-                    frames_per_buffer=frames_per_buffer)
-
-    print(f"Recording from device {device_index} for {duration} seconds...")
-
-    frames = []
-
-    for _ in range(0, int(rate / frames_per_buffer * duration)):
-        data = stream.read(frames_per_buffer)
-        frames.append(data)
-
-    print("Recording finished.")
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open(filename, 'wb')
-    wf.setnchannels(channels)
-    wf.setsampwidth(p.get_sample_size(format))
-    wf.setframerate(rate)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-
-if __name__ == "__main__":
-    mics = list_microphones()
-    if mics:
-        print("Доступні мікрофони:")
-        for mic in mics:
-            print(f"ID: {mic[0]}, Назва: {mic[1]}")
-
-        # Запис аудіо з кожного мікрофона
-        for mic in mics:
-            mic_id, mic_name = mic
-            filename = f"mic_{mic_id}.wav"
-            record_audio(mic_id, duration=5, filename=filename)
-            print(f"Audio recorded from {mic_name} saved as {filename}")
-    else:
-        print("Не знайдено доступних мікрофонів.")
+outputs = model.generate(**input_ids, max_new_tokens=500)
+print(tokenizer.decode(outputs[0]))
