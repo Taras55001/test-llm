1c0f36b4b0117d47be775c285cc331f1b53540f0
taras55001 <taras5500@gmail.com>
2024-06-02 16:36:02 +0200

    new test

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-1.1-2b-it")
 model = AutoModelForCausalLM.from_pretrained(
-    "google/gemma-2b",
+    "google/gemma-1.1-2b-it",
+    device_map="auto",
     torch_dtype=torch.bfloat16
 )
 
 input_text = "Write me a poem about CNC Machinist."
-input_ids = tokenizer(input_text, return_tensors="pt")
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda"if torch.cuda.is_available() else "cpu")
 
 outputs = model.generate(**input_ids, max_new_tokens=500)
 print(tokenizer.decode(outputs[0]))
