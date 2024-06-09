9812a33d4768ff838d75f49597d5d759baff4575
taras55001 <taras5500@gmail.com>
2024-06-09 17:34:13 +0200

    update test

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import os
+import subprocess
 
 local_model_path = "./gemma-1.1-2b-it"
+dir_exists = os.path.exists(local_model_path)
+if not dir_exists:
+    clone_command = ["git", "clone", "https://huggingface.co/google/gemma-1.1-2b-it"]
+    subprocess.run(clone_command)
+
 
 def llm_request(input_text, max_token):
     tokenizer = AutoTokenizer.from_pretrained(local_model_path)
